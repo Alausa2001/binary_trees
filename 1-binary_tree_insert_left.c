da5c001e3b4622fcfe69ed8b21b1c84b067516ea
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-21 01:53:09 +0100

    adds node to the left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts value to the left branch of a binary tree
+ * @parent: the node of the tree
+ * @value: the value to be inserted into the tree
+ * Return: a pointer to the created node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *leftnode;
+
+
+	if (parent == NULL)
+		return (NULL);
+
+	leftnode = binary_tree_node(parent, value);
+	if (leftnode == NULL)
+		return (NULL);
+
+
+	if (parent->left != NULL)
+	{
+		leftnode->left = parent->left;
+		parent->left->parent = leftnode;
+	}
+	parent->left = leftnode;
+	return (leftnode);
+
+}
