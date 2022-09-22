ba5512404dbd87117b10c5c9f8b2866ffc3d9a57
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-22 08:51:18 +0100

    corrected the code (node is a pointer to the node o check not to te tree)

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -12,9 +12,4 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
-	if (node != NULL)
-	{
-		binary_tree_is_leaf(node->left);
-		binary_tree_is_leaf(node->left);
-	}
 }
