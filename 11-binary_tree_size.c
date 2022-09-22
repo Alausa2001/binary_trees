2b4830b99ad53442b0f1ad7b29a95d4ac26014b6
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-22 10:13:15 +0100

    function that calculates the size of a tree/subtree in a tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,19 +1,20 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - calculates the height of a tree
+ * binary_tree_size - calculates the height of a tree
  * @tree: pointer to root node
- * Return: the height of the tree
+ * Return: the size of the tree/subtree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	int count_no_nodes;
+	int no_nodes;
 
 	if (tree == NULL)
 		return (0);
 
+
 	if (tree != NULL)
 	{
-		count_no_nodes = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+		no_nodes = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 	}
-	return (count_no_nodes);
+	return (no_nodes);
 }
