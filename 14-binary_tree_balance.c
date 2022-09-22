bf35c87ad156becc144b071b249935c8e5b7b863
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-22 17:34:26 +0100

    funtion that calculates the balance factor of a tree/subtree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,23 +19,12 @@ int binary_tree_balance(const binary_tree_t *tree)
 		count_l += 1;
 
 	}
+	count_l -= 1;
 	while (right != NULL)
 	{
 		right = right->right;
 		count_r++;
 	}
+	count_r -= 1;
 	return (count_l - count_r);
-}
-/**
- * binary_tree_is_leaf - function checks is a node is a leafnode
- * @node: pointer to the node
- * Return: 1 if node is a leafnode else 0
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-	if (node->left == NULL && node->right == NULL)
-		return (1);
-	return (0);
 }
