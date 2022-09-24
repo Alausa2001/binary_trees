b74ce81f7451d5392704b9c8c5808db8c114e562
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-24 08:43:51 +0100

    returns the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,26 +8,38 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *temp = node;
+	binary_tree_t *uncle;
 
-	if (node == NULL || node->parent->parent == NULL)
-		return (NULL);
-	if (node->parent == NULL)
+	if (node == NULL || (temp->parent == NULL))
 		return (NULL);
 	if (temp != NULL)
 	{
-		temp = temp->parent->parent;
-		if (temp->right->right == node || temp->right->left == node)
-		{
-			if (temp->left != NULL)
-				return (temp->left);
-			return (NULL);
-		}
-		if (temp->left->left == node || temp->left->right == node)
-		{
-			if (temp->right != NULL)
-				return (temp->right);
-			return (NULL);
-		}
+		temp = temp->parent;
+		uncle = binary_tree_sibling(temp);
+		return (uncle);
 	}
 	return (NULL);
 }
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - return the sibling of a node
+ * @node : pointer to node whose sibling is to be determined
+ * Return: the sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *temp = node;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (temp != NULL)
+	{
+		temp = temp->parent;
+		if (temp->right == node)
+			return (temp->left);
+		if (temp->left == node)
+			return (temp->right);
+	}
+	return (NULL);
+}
+
