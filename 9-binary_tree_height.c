b2c02a1fa33b539eba1dd3ab0f715a48a60fc3e7
Alausa Abdulqoyum Oluwaferanmi <101368817+Alausa2001@users.noreply.github.com>
2022-10-11 21:52:55 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
+	
 	if (tree != NULL)
 	{
 		count_l = 1 + binary_tree_height(tree->left);
